b9f12dd4860d61ecdb6c95b9d8a6adca7bfe3e1a
Evan Rees <err87@cornell.edu>
2021-03-17 12:18:28 -0400

    backwards compatibility w/ 1.3

--- kotlin/src/main/kotlin/MagnetParticles.kts
+++ kotlin/src/main/kotlin/MagnetParticles.kts
@@ -4,4 +4,4 @@ fun v(k: Int, n: Int): Double = 1 / (k * (n + 1.0).pow(2 * k))
 
 fun u(k: Int, n: Int) = (1..n).sumByDouble { v(k, it) }
 
-fun doubles(maxk: Int, maxn: Int): Double = (1..maxk).sumOf { u(it, maxn) }
+fun doubles(maxk: Int, maxn: Int): Double = (1..maxk).map { u(it, maxn) }.sum()
