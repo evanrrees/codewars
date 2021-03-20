4a347b87600a9c074745c3fe58f965f3b1d64c56
Evan Rees <err87@cornell.edu>
2021-03-20 12:55:56 -0400

    add years

--- kotlin/src/main/kotlin/HumanReadableDurationFormat.kts
+++ kotlin/src/main/kotlin/HumanReadableDurationFormat.kts
@@ -1,24 +1,23 @@
 object TimeFormatter {
-    fun formatDuration(seconds: Int): String {
-        val components = mutableMapOf<String, Int>().apply {
+    fun formatDuration(seconds: Int): String =
+        if (seconds == 0) "now"
+        else mutableMapOf<String, Int>().apply {
             var s = seconds
-            set("days", s / (24 * 3600))
-            s %= (24 * 3600)
-            set("hours", s / 3600)
-            s %= 3600
+            set("years", s / (365 * 24 * 60 * 60))
+            s %= (365 * 24 * 60 * 60)
+            set("days", s / (24 * 60 * 60))
+            s %= (24 * 60 * 60)
+            set("hours", s / (60 * 60))
+            s %= (60 * 60)
             set("minutes", s / 60)
             s %= 60
             set("seconds", s)
         }
             .filterValues { it > 0 }
             .map { (k, v) -> "$v ${if (v == 1) k.dropLast(1) else k}" }
-
-        return when (components.size) {
-            4 -> "${components[0]}, ${components[1]}, ${components[2]} and ${components[3]}"
-            3 -> "${components[0]}, ${components[1]} and ${components[2]}"
-            2 -> "${components[0]} and ${components[1]}"
-            1 -> components[0]
-            else -> "now"
-        }
-    }
+            .run {
+                if (size == 1) this
+                else dropLast(2) + takeLast(2).let { (a, b) -> "$a and $b" }
+            }
+            .joinToString(", ")
 }
